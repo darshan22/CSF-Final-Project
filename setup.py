ee4efd9212b9600aed54dd1f62f9a42096635676
Darshan Kasat <darshankasat22@gwu.edu>
2018-12-07 00:26:27 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md","r") as f:
+	long_description = f.read()
+
+setup(name = "refiner",
+	version = "1.0",
+	author = "Darshan Kasat & Shivam Thassu",
+	author_email = "darshankasat22@gwu.edu",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/darshan22/CSF-Final-Project",
+	packages = setuptools.find_packages(),
+	classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",],
+)
+	
+	
