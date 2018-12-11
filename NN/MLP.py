ac926646c60d4800fb16a01f6c211b8a2c8cfd56
Shivam Thassu <shivamtv2@gmail.com>
2018-12-10 22:38:39 -0500

    Take activation function from the user

--- NN/MLP.py
+++ NN/MLP.py
@@ -3,17 +3,30 @@ import torch.nn as nn
 
 #define class for MLP
 class Net(nn.Module):
-    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
+    def __init__(self, input_size, hidden_size, num_classes, activation):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size1)
-        self.relu1 = nn.ReLU()
+        self.relu1 = activation
         self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-        self.relu2 = nn.ReLU()
+        self.relu2 = activation
         self.fc3 = nn.Linear(hidden_size2, num_classes)
 
     def forward(self, x):
         out = self.fc1(x)
-        out = self.relu1(out)
+        if self.activation == "sigmoid":
+           sigmoid = nn.Sigmoid()
+           out = sigmoid(out)
+       elif self.activation == "relu":
+           relu = nn.ReLU()
+           out = relu(out)
+       elif self.activation == "tanh":
+           tanh = nn.Tanh()
+           out = tanh(out)
+       elif self.activation == "softmax":
+           soft_max = nn.Softmax()
+           out = soft_max(out)
+       else:
+           pass
         out = self.fc2(out)
         out = self.relu2(out)
         out = self.fc3(out)
