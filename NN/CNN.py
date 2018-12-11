cfae6bb72eeee88b5728ad877949e8fa3a11aa85
Shivam Thassu <shivamtv2@gmail.com>
2018-12-10 23:17:05 -0500

    Add third layer

--- NN/CNN.py
+++ NN/CNN.py
@@ -15,11 +15,17 @@ class CNN(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
         self.fc = nn.Linear(7 * 7 * 32, 10)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
