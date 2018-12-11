0a2f72a50977e65e150b008fe0381a287c9a29e4
Darshan Kasat <darshankasat22@gwu.edu>
2018-12-10 22:30:48 -0500

    make the network to accept the layer sizes

--- NN/MLP.py
+++ NN/MLP.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
 
-class MLP(nn.Module):
-    def __init__(self):
-        super(MLP, self).__init__()
-        self.l1 = nn.Linear(1 * 28 * 28, 20)
-        self.t1 = nn.Tanh()
-        self.l2 = nn.Linear(20, 10)
-        self.t2 = nn.LogSoftmax(dim=1)
+#define class for MLP
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size2, num_classes)
 
     def forward(self, x):
-        x = x.view(-1, 1 * 28 * 28)
-        x = self.t1(self.l1(x))
-        x = self.t2(self.l2(x))
-return x
+        out = self.fc1(x)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        out = self.relu2(out)
+        out = self.fc3(out)
+        return out
